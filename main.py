7f3c8be22c3756566aca461670a9745c031769d1
JvQueiros <joao.victor.queiros@hotmail.com>
2025-07-03 07:03:05 -0300

    .

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import os
 import json
+import boto3 as b3
+
 
 with open('config.json') as conf:
     config = json.load(conf)
 
-file_path = os.path.join(config['uri'], 'teste.txt')
+uri = config['uri']
+
+s3 = b3.connect('s3',
+                region_name = 'code_region',
+                aws_access_key_id = key723474r
+                aws_secret_access_key = asdfasdf
+
+)
+
+
+for file in uri:
